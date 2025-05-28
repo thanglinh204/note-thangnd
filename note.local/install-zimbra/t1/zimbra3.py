baa37d8d9d22dd4e9e85f9f6264606fe54b06d09
frankie-nguyen <45789067+thanglinh204@users.noreply.github.com>
2025-05-28 10:12:28 +0700

    Delete note.local/install-zimbra/t1 directory

--- note.local/install-zimbra/t1/zimbra3.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def main():
-    print("\n🚀 Bắt đầu trình cài đặt Zimbra. Làm theo hướng dẫn hiện ra trên màn hình...")
-    os.chdir("zimbra/zcs-8.8.15_GA_3869.RHEL7_64.20190918004220")
-    os.system("./install.sh")
-
-if __name__ == "__main__":
-    main()
